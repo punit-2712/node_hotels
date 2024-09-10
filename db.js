05ac90a49547c4d5958769128c0fe6b83e38e151
Punit Vachhani <40193957+punit-2712@users.noreply.github.com>
2024-09-10 09:30:11 +0530

    Other version

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const mongoURL = "mongodb://localhost:27017/hotels ";
+//const mongoURL = "mongodb://localhost:27017/hotels ";
+const mongoURL =
+  "mongodb+srv://vachhanipunit:Punit0!21$@cluster0.cyz0m.mongodb.net/";
 
 mongoose.connect(mongoURL, {
   useNewUrlParser: true,
